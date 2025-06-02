cd21cce72acce4ac5befc6d3f9331135822d137e
RoaFayad <rf.phys2@gmail.com>
2025-06-02 04:46:10 +0300

    feat: add new input lines asking for user information

---
+++ assignment1.py
@@ -0,0 +1,8 @@
+#Ask user for informations
+print("Please enter the following information:")
+name = input("Your name in English: ")
+age = float(input ("Your age:"))
+gpa = float(input(Your GPA (between 0 and 5): ))
+foi = input("Your field of interest: ")
+graduation_status = input("Wethere you're graduated or not(just answer by yes or no): ")
+
