1b0fb567bf569eb2c638d4f07fea185ed9434f65
RoaFayad <rf.phys2@gmail.com>
2025-06-02 04:59:03 +0300

    feat: checking information for GPA and graduation status

--- assignment1.py
+++ assignment1.py
@@ -6,3 +6,11 @@ gpa = float(input(Your GPA (between 0 and 5): ))
 foi = input("Your field of interest: ")
 graduation_status = input("Wethere you're graduated or not(just answer by yes or no): ")
 
+#Checking some given information
+if gpa<0 or gpa>5:
+    gpa = float(input("Re-enter your GPA please, it must be between 0 and 5: "))
+    
+if graduation_status.lower() != "yes" or graduation_status.lower() != "no":
+    graduation_status = input("Please answer wethere you have graduated or not wiht only "yes" or "no": ")
+
+    
